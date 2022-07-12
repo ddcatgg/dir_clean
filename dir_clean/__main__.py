bbe4c65c0d1f819b826b304eac591d7e54a68985
jun.chen <jun.chen@meetsocial.cn>
2022-07-12 20:49:23 +0800

    0.1.1 - level=logging.INFO

--- dir_clean/__main__.py
+++ dir_clean/__main__.py
@@ -5,7 +5,7 @@ import logging
 from .dir_clean import main
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+    logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     ret = main()
     sys.exit(ret)
